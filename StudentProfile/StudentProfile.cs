98eb62ad0f38da343f004edb1c2ddb0594a9a0db
TanGutz <TanGutz@DESKTOP-LSU5CF3>
2026-09-14 00:17:30 +0800

    Add StudentProfile Visual Studio project

---
+++ StudentProfile/StudentProfile.cs
@@ -0,0 +1,10 @@
+namespace StudentProfile
+{
+    public partial class StudentProfile : Form
+    {
+        public StudentProfile()
+        {
+            InitializeComponent();
+        }
+    }
+}
